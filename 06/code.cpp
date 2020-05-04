3b670715f4c2a008071cd5d440e17bff69b3a8db
Zhanyu Wu <wua@mail.smu.edu>
2020-05-03 21:57:18 -0500

    Update code.cpp

--- 06/code.cpp
+++ 06/code.cpp
@@ -25,5 +25,58 @@ public:
 		if (input == "0")
 			return "0101010";
 		else if (input == "1")
+			return "0111111";
+		else if (input == "-1")
+			return "0111010";
+		else if (input == "D")
+			return "0001100";
+		else if (input == "A")
+			return "0110000";
+		else if (input == "M")
+			return "1110000";
+		else if (input == "!D")
+			return "0001101";
+		else if (input == "!A")
+			return "0110001";
+		else if (input == "!M")
+			return "1110001";
+		else if (input == "-D")
+			return "0001111";
+		else if (input == "-A")
+			return "0110011";
+		else if (input == "-M")
+			return "1001111";
+		else if (input == "D+1")
+			return "0011111";
+		else if (input == "A+1")
+			return "0110111";
+		else if (input == "M+1")
+			return "1110111";
+		else if (input == "D-1")
+			return "0001110";
+		else if (input == "A-1")
+			return "0110010";
+		else if (in == "M-1")
+			return "1110010";
+		else if (input == "D+A")
+			return "0000010";
+		else if (input == "D+M")
+			return "1000010";
+		else if (input == "D-A")
+			return "0010011";
+		else if (input == "D-M")
+			return "1010011";
+		else if (input == "A-D")
+			return "0000111";
+		else if (input == "M-D")
+			return "1000111";
+		else if (input == "D&A")
+			return "0000000";
+		else if (input == "D&M")
+			return "1000000";
+		else if (input == "D|A")
+			return "0010101";
+		else if (input == "D|M")
+			return "1010101";
 		}
 };
